fb6ec0bc9b298dd4489b9f3cdce63585100716b4
tahmin415 <tahmin415@gmail.com>
2024-10-27 16:53:54 +0330

    nahayi

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-print("hello")
+class CustomStr:
+    def __init__(self, string):
+        self.string = string
 
-def hello():
-     print(hello)
+    def __str__(self):
+        return self.string
 
-print('hello world')
+    def __getitem__(self, index):
+        return self.string[index]
+
+    def __setitem__(self, index, value):
+        list = list(self.string)
+        list[index] = value
+
+        return "".join(list)
+
+    def custom_split(self, *splits):
+        split_string = [self.string]
+        for i in splits:
+            a = []
+            for q in split_string:
+                a.extend(q.split(i))
+            split_string = a
+
+        return split_string
+
+    def remove_duplicate(self):
+        char_see = set()
+        result = []
+
+        for i in self.string:
+            if i not in char_see:
+                char_see.add(i)
+                result.append(i)
+
+        return "".join(result)
+
+    def is_float(self):
+        try:
+            float(self.string)
+            return True
+        except:
+            return False
+
+    def is_palindrome(self):
+        if self.string == self.string.reversed():
+            return True
+        return False
